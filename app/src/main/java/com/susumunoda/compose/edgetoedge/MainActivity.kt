9ecf70660f3ddf152f0df0410a451c76dfbdf792
Susumu Noda <susumunoda@gmail.com>
2023-09-13 16:31:48 -0700

    Add background color to more clearly show edge-to-edge UI

--- app/src/main/java/com/susumunoda/compose/edgetoedge/MainActivity.kt
+++ app/src/main/java/com/susumunoda/compose/edgetoedge/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
@@ -22,9 +23,13 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             EdgeToEdgeTheme {
-                Scaffold { contentPadding ->
+                Scaffold(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                ) { contentPadding ->
                     Box(
-                        modifier = Modifier.fillMaxSize().padding(contentPadding),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(contentPadding),
                         contentAlignment = Alignment.Center
                     ) {
                         Text("Edge to edge UI")
